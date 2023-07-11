a85229e81cb238b10b1b61dc00b594a1f8b09e41
Nilo Freitas <nilo.freitas@agenciam3.com>
2023-07-10 23:16:49 -0300

    initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react-native';
+
+import { View, Text } from 'react-native';
+
+export function Home() {
+  return (
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>React Native</Text>
+        <Text>Rocketseat Ignite</Text>
+      </View>
+    </>
+  );
+}
